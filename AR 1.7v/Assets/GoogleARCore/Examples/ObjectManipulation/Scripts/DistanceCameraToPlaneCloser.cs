0c195b8079063a6f71ba6a2fe20535b94a2ce9f9
Umberto-PC\Umberto <okamirvs@gmail.com>
2019-04-02 01:52:11 +0200

    Fix text menu

---
+++ AR 1.7v/Assets/GoogleARCore/Examples/ObjectManipulation/Scripts/DistanceCameraToPlaneCloser.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TMPro {
+
+    public class DistanceCameraToPlaneCloser : MonoBehaviour {
+
+        public GameObject distance;
+        Camera main;
+        float minDistance;
+
+        // Use this for initialization
+        void Start() {
+            distance.GetComponent<TextMeshProUGUI>().text = "";
+            main = Camera.main;
+            minDistance = Mathf.Infinity;
+        }
+
+        // Update is called once per frame
+        void Update() {
+            foreach (GameObject plane in GameObject.FindGameObjectsWithTag("plane")) {
+                Vector3 tmp = plane.transform.position;
+                float distance = Vector3.Distance(main.transform.position, tmp);
+                if (distance < minDistance) {
+                    minDistance = distance;
+                }
+            }
+            distance.GetComponent<TextMeshProUGUI>().text = minDistance.ToString();
+
+
+        }
+    }
+}
